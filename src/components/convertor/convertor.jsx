a7e394c9ec1bc76f68eb3f9125261749afef42ae
dimitriktm <dimitri32023@gmail.com>
2020-03-10 21:06:50 +0100

    convertor component logic

--- src/components/convertor/convertor.jsx
+++ src/components/convertor/convertor.jsx
@@ -8,64 +8,131 @@ import {
 	Grid
 } from 'semantic-ui-react'
 import './convertor.css';
+import convert from 'convert-units'
 /**
 * Class representing a real time table of measurements
 *
 * Real time table of measurements where you can input value and see converted value in all units of table
 */
 export class Convertor extends Component {
-	state = {selectedTable: 'length'};
-	tablesDropdown = [
-		{key: 'length', text: 'Length', value: 'length'},
-		{key: 'time', text: 'Time', value: 'time'}
-	];
-	tables = {
-		length: {
-			getListOfUnits: function() {
-				return [
-					{abbr: 'mm', fullName: 'Milimeters'},
-					{abbr: 'cm', fullName: 'Centimeters'},
-					{abbr: 'km', fullName: 'KiloMeters'}
-				]
-			}
-		},
-		time: {
-			getListOfUnits: function() {
-				return [
-					{abbr: 's', fullName: 'Seconds'},
-					{abbr: 'm', fullName: 'Minutes'},
-					{abbr: 'h', fullName: 'Hours'}
-				]
-			}
-		}
-	}
+	state = {selectedTable: '', table: [], unit: '', value: 1};
 
+	/** data */
 	getListOfUnits() {
-		const {selectedTable} = this.state;
-		const {tables} = this;
+		return convert().list(this.state.selectedTable);
+	}
+	getListOfMeasures() {}
+	getDropdownListOfUnits() {
+		let unitsList,
+			dropdownUnitsList;
 
-
-		let listOfUnits = new Array();
-
-		tables[selectedTable].getListOfUnits().forEach(unit => {
-			listOfUnits.push(
+		unitsList = convert().list(this.state.selectedTable);
+		dropdownUnitsList = new Array();
+		for(let unit of unitsList) {
+			dropdownUnitsList.push(
 				{
 					key: unit.abbr,
-					text: unit.fullName,
+					text: unit.plural,
 					value: unit.abbr
 				}
-			)
-		})
+			);
+		}
 
-		return listOfUnits;
+		return dropdownUnitsList;
 	}
+	getDropdownMeasuresList() {
+		let measuresList,
+			dropdownMeasuresList;
 
+		measuresList = convert().measures();
+		dropdownMeasuresList = new Array();
+		for(let measure of measuresList) {
+			dropdownMeasuresList.push(
+				{
+					key: measure,
+					text: measure,
+					value: measure
+				}
+			)
+		}
+
+		return dropdownMeasuresList;
+	}
+	/* Handlers */
+
+	/**
+	* when user selects table we change measurement table and set unit to first
+	* unit in measurament table
+	*/
 	onTableSelect(event, { value }) {
 		this.setState({
-			selectedTable: value
+			selectedTable: value,
+			unit: convert().list(value)[0].abbr
 		});
 	}
-	onValueChange({ target: { value } }) {}
+	onValueChange({ target: { value } }) {
+		this.setState(
+			{
+				value: value
+			}
+		)
+	}
+	onUnitChange(event, { value }) {
+		this.setState(
+			{
+				unit: value
+			}
+		)
+	}
+	onConvert() {
+		let results;
+
+		results = this.convertValue(this.state.value, this.state.unit)
+		this.setState(
+			{
+				table: results
+			}
+		)
+	}
+	convertValue(value, unit) {
+		if(!value || !unit) {return false;}
+		let results,
+			unitsList;
+
+		results = new Array();
+		unitsList = this.getListOfUnits();
+
+		for(let unit of unitsList)
+		{
+			results.push(
+				{
+					value: convert(this.state.value).from(this.state.unit).to(unit.abbr),
+					...unit
+				}
+			)
+		};
+
+		return results;
+	}
+	componentDidMount() {
+		let defaultUnit,
+			defaultValue,
+			defaultSelectedTable,
+			defaultTable;
+
+		defaultUnit = 'mm';
+		defaultValue = 1;
+		defaultSelectedTable = 'length';
+		defaultTable = this.convertValue(defaultValue, defaultUnit);
+
+		this.setState({
+			selectedTable: defaultSelectedTable,
+			table: defaultTable,
+			unit: defaultUnit,
+			value: defaultValue
+		});
+
+	}
 	render() {
 		const {
 			tablesDropdown
@@ -73,17 +140,15 @@ export class Convertor extends Component {
 		const {
 			selectedTable
 		} = this.state;
-
-		const listOfUnits = this.getListOfUnits();
-		const defaultValue = listOfUnits[1].value;
-		const dropdownLabel = <Dropdown className='purple' defaultValue={defaultValue} options={listOfUnits} />;
+		const unitsList = this.getDropdownListOfUnits();
+		const dropdownLabel = <Dropdown onChange={this.onUnitChange.bind(this)} scrolling className='purple' value={this.state.unit} options={unitsList}/>;
 		return (
 			<div className='wrapper'>
 				<Container text>
 					<Grid columns='equal'>
 						<Grid.Row>
 							<Grid.Column>
-							<Dropdown onChange={this.onTableSelect.bind(this)} className='label purple' fluid defaultValue={selectedTable} options={tablesDropdown} />
+							<Dropdown onChange={this.onTableSelect.bind(this)} className='label purple' fluid scrolling value={selectedTable} options={this.getDropdownMeasuresList()} />
 							</Grid.Column>
 						</Grid.Row>
 						<Grid.Row>
@@ -98,20 +163,19 @@ export class Convertor extends Component {
 								/>
 							</Grid.Column>
 							<Grid.Column>
-								<Button size='small' className='purple'>Convert</Button>
+								<Button onClick={this.onConvert.bind(this)} size='small' className='purple'>Convert</Button>
 							</Grid.Column>
 						</Grid.Row>
 					</Grid>
 					<Table definition>
 						<Table.Body>
-						  <Table.Row>
-							<Table.Cell>Millimeters</Table.Cell>
-							<Table.Cell>300</Table.Cell>
-						  </Table.Row>
-						  <Table.Row>
-							<Table.Cell>Kilometers</Table.Cell>
-							<Table.Cell>200</Table.Cell>
-						  </Table.Row>
+
+							{this.state.table.map(({ plural, value }) =>
+								<Table.Row key={plural}>
+									<Table.Cell>{ plural }</Table.Cell>
+									<Table.Cell>{ value }</Table.Cell>
+								</Table.Row>
+							)}
 						</Table.Body>
 					</Table>
 				</Container>
